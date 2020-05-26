2e84212d4862f1a99d51fc356f1e12281f07d5df
SushilBalhotra <50832625+SushilBalhotra@users.noreply.github.com>
2020-05-26 16:16:57 +0530

    second microservice changes

---
+++ SecondSpringProject/src/main/java/com/first/demo/FirstSpringProject/SecondSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.first.demo.FirstSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringProjectApplication.class, args);
+	}
+
+}
---
+++ SecondSpringProject/src/main/java/com/first/demo/FirstSpringProject/rest/FirstRestController.java
@@ -0,0 +1,34 @@
+package com.first.demo.FirstSpringProject.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstRestController {
+
+	
+	@Value("${coach.name}")
+	String coachName;
+	
+	@Value("${team.name}")
+	String teamName;
+	
+	@GetMapping("/teamInfo")
+	public String getTeamInfo()
+	{
+		return "Coach Name :"+coachName + "Team Name :"+teamName;
+	}
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello world : this is second microservice, be safe and local time is :: " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/fortunes")
+	public String sayFortune() {
+		return "Today is my lucky day..... ";
+	}
+
+}
